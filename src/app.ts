c57b2d4c6bf6a7b14d00ad891c3d0cc07cc2fdd5
mashayeakh <islammasayekh@gmail.com>
2025-12-29 14:27:00 +0600

    authentication done with better auth

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from "cors";
 import { PostRouter } from "./modules/post/post.router";
 import { RouteNotFoundRouter } from "./modules/404/404.routes";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+
 const app = express();
 
 app.use(express.json());
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 //root route
 app.get("/", (req, res) => {
