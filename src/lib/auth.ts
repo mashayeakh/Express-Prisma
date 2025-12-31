3ec65420da25eeccd2fefd6e663bb0da330a3354
mashayeakh <islammasayekh@gmail.com>
2025-12-31 14:41:34 +0600

    configured google auth service

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -130,4 +130,12 @@ export const auth = betterAuth({
             }
         },
     },
+
+    //goolge auth options
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
 });
